e5b722962e006156f508924833dc88cf3d1cfd6a
Greta <g.a.skorupska-ruiz@student.tudelft.nl>
2023-02-20 09:03:00 +0100

    abandoned

--- data reading.py
+++ data reading.py
@@ -1,5 +1,6 @@
 import scipy.io
 import numpy as np
+import datetime
 
 data = scipy.io.loadmat("B0005.mat")
 data = data.items()
@@ -8,39 +9,77 @@ data = list(data)
 
 data = np.array(data, dtype = object)
 data = data[3][1][0][0][0][0]
-print(data)
+#initial data filtering above
 
+allcycles = []
 
-all_tests = []
+for i in range(len(data)+1):
+    onecycle = []
+    
+    data = data[i]
+    print(data)
+    cycle = data[0][0]
+    cycle_list = np.full(len(data[3][0][0][0][0]), cycle)
 
-test = []
+    temp = data[1][0][0]
+    temp_list = np.full(len(data[3][0][0][0][0]), temp)
 
-data = data[0]
+    date = data[2][0]
+    date_time = datetime.datetime(int(date[0]),
+        int(date[1]),
+        int(date[2]),
+        int(date[3]),
+        int(date[4])) + datetime.timedelta(seconds=int(date[5]))
+    date_list = np.full(len(data[3][0][0][0][0]), date_time)
 
-cycle = data[0][0]
-temp = data[1][0][0]
-date = data[2][0]
+    data = data[3][0][0]
 
-data = data[3][0][0]
-
-voltage = data[0][0]
-current = data[1][0]
-temperature = data[2][0]
-current_charge = data[3][0]
-voltage_charge = data[4][0]
-time = data[5][0]
-
-test.append(cycle)
-test.append(temp)
-test.append(date)
-test.append(voltage)
-test.append(current)
-test.append(temperature)
-test.append(current_charge)
-test.append(voltage_charge)
-test.append(time)
-all_tests.append(test)
-
-#print(mode)
-#print(temp)
-#print(time)
+    if cycle == 'charge':
+        voltage = np.array(data[0][0])
+        current = np.array(data[1][0])
+        temperature = np.array(data[2][0])
+        current_charge = np.array(data[3][0])
+        voltage_charge = np.array(data[4][0])
+        time = np.array(data[5][0])
+        onecycle.append(voltage)
+        onecycle.append(current)
+        onecycle.append(temperature)
+        onecycle.append(current_charge)
+        onecycle.append(voltage_charge)
+        onecycle.append(time)
+        
+    elif cycle == 'discharge':
+        voltage = np.array(data[0][0])
+        current = np.array(data[1][0])
+        temperature = np.array(data[2][0])
+        current_load = np.array(data[3][0])
+        voltage_load = np.array(data[4][0])
+        time = np.array(data[5][0])
+        capacity = np.array(data[6][0])
+        onecycle.append(voltage)
+        onecycle.append(current)
+        onecycle.append(temperature)
+        onecycle.append(current_load)
+        onecycle.append(voltage_load)
+        onecycle.append(time)
+        onecycle.append(capacity)
+        
+    elif cycle == 'impedance':
+        sense_current = np.array(data[0][0])
+        battery_current = np.array(data[1][0])
+        current_ratio = np.array(data[2][0])
+        battery_impedance = np.array(data[3][0])
+        rectified_impedance = np.array(data[4][0])
+        re = np.array(data[5][0])
+        rct = np.array(data[6][0])
+        onecycle.append(sense_current)
+        onecycle.append(battery_current)
+        onecycle.append(current_ratio)
+        onecycle.append(battery_impedance)
+        onecycle.append(rectified_impedance)
+        re_list = np.full(len(sense_current), re)
+        rct_list = np.full(len(battery_current), rct)
+        onecycle.append(re_list)
+        onecycle.append(rct_list)
+    
+    allcycles.append(onecycle)
