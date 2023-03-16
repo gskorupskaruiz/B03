cd5f5903f1ca7504a5008d6efc0335ec6e1b506c
AlexisVandenHeede <105492216+AlexisVandenHeede@users.noreply.github.com>
2023-03-16 09:52:53 +0100

    should load from mat files now

--- PytorchLSTM/main.py
+++ PytorchLSTM/main.py
@@ -18,22 +18,24 @@ def load_data(battery):
                                         int(row['time'][0][2]),
                                         int(row['time'][0][3]),
                                         int(row['time'][0][4])) + datetime.timedelta(seconds=int(row['time'][0][5]))
-        data = row['data']
-        capacity = data[0][0]['Capacity'][0][0]
-        for j in range(len(data[0][0]['Voltage_measured'][0])):
-            voltage_measured = data[0][0]['Voltage_measured'][0][j]
-            current_measured = data[0][0]['Current_measured'][0][j]
-            temperature_measured = data[0][0]['Temperature_measured'][0][j]
-            current_load = data[0][0]['Current_load'][0][j]
-            voltage_load = data[0][0]['Voltage_load'][0][j]
-            time = data[0][0]['Time'][0][j]
-            dataset.append([counter + 1, ambient_temperature, date_time, capacity,
-                            voltage_measured, current_measured,
-                            temperature_measured, current_load,
-                            voltage_load, time])
-        capacity_data.append([counter + 1, ambient_temperature, date_time, capacity])
-        counter += 1
+            data = row['data']
+            capacity = data[0][0]['Capacity'][0][0]
+            for j in range(len(data[0][0]['Voltage_measured'][0])):
+                voltage_measured = data[0][0]['Voltage_measured'][0][j]
+                current_measured = data[0][0]['Current_measured'][0][j]
+                temperature_measured = data[0][0]['Temperature_measured'][0][j]
+                current_load = data[0][0]['Current_load'][0][j]
+                voltage_load = data[0][0]['Voltage_load'][0][j]
+                time = data[0][0]['Time'][0][j]
+                dataset.append([counter + 1, ambient_temperature, date_time, capacity,
+                                voltage_measured, current_measured,
+                                temperature_measured, current_load,
+                                voltage_load, time])
+            capacity_data.append([counter + 1, ambient_temperature, date_time, capacity])
+            counter += 1
     print(dataset[0])
+    print(capacity_data[0])
+
     return [pd.DataFrame(data=dataset,
                         columns=['cycle', 'ambient_temperature', 'datetime',
                                     'capacity', 'voltage_measured',
