3f37f729c835a554c469a63b2ab172c6c54d57d9
AlexisVandenHeede <105492216+AlexisVandenHeede@users.noreply.github.com>
2023-03-16 18:28:10 +0100

    Added loading data from csv, still need some processing

--- PytorchLSTM/main.py
+++ PytorchLSTM/main.py
@@ -1,16 +1,20 @@
 import torch
 import datetime
 import pandas as pd
-from scipy.io import loadcsv
 
+data_fields = {
+        'Voltage_measured', 'Current_measured', 'Temperature_measured',
+        'Current_charge', 'Voltage_charge', 'Time', 'Capacity'}
 
 def load_data(battery): #read csv save a torch tensors
-	data = loadcsv("data/" + battery + ".csv") #ask Twan for name of csv with TTD
-
+	data = pd.read_csv("data/" + battery + ".csv") #ask Twan for name of csv with TTD
 	print(data)
-	
-	
-
+	print(data.dtypes)
+	data = data.drop(data[data.type != "discharge"].index) # only keep discharge data
+	data = data.drop(["start_time", "type", "Sense_current", 'Battery_current', 'Current_ratio', 
+		  'Battery_impedance', 'Rectified_impedance', 'Re', 'Rct'], axis=1) # removes columns that are used for impedance, type and start time
+	print(data.dtypes)	
+	print(data)
 
 
 def main():
