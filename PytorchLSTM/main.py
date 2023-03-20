8623415e175dea1674267d37d4e42e73f4316cf0
AlexisVandenHeede <105492216+AlexisVandenHeede@users.noreply.github.com>
2023-03-20 11:28:20 +0100

    main lstm

--- PytorchLSTM/main.py
+++ PytorchLSTM/main.py
@@ -3,16 +3,14 @@ from sklearn.model_selection import train_test_split
 from model import *
 from sklearn import preprocessing
 import numpy as np
-from sklearn.preprocessing import StandardScaler
+from data_processing import load_gpu_data
 
-def load_data_split_normalise(battery):
+def load_data_normalise(battery):
 
     data = pd.read_csv("data/" + battery + "_TTD.csv")
-    scale = StandardScaler()
-    data = scale.fit_transform(data)
-    return data
+    normalized_data = (data-data.mean())/data.std()
+    return normalized_data
     
-
 
 def train_test_validation_split(X, y, test_size, cv_size):
     """
@@ -48,19 +46,31 @@ def testing_func(num, group_for_test):
     rmse_test = (np.sqrt(rmse_test / num)).item()
     return result_test, rmse_test
 
-def train(model, num_train, group):
+def train(model, battery):
+
      # number of samples
      # grouped data per sample
 
-     rmse_temp = 1000
+    norm_data = load_data_normalise(battery)
+    
+    X = norm_data.drop("TTD", axis = 1)
+    y = norm_data["TTD"]
+    X_train, y_train, X_test, y_test, X_cv, y_cv  = train_test_validation_split(X, y, test_size, cv_size)
+    num_train = len(X_train.index)
 
-     for epoch in range(1, n_epoch +1):
+    rmse_temp = 1000
+
+    for epoch in range(1, n_epoch +1):
 
         model.train()
         epoch_loss = 0
 
         for i in range(1, num_train +1):
             # stuff
+            X_current = X_train[i]
+            y_current = y_train[i]
+            load_gpu_data(X_current, y_current)
+
 
             outputs = model(X_train) # forward pass
             optimizer.zero_grad() # calc and set grad = 0
@@ -72,7 +82,7 @@ def train(model, num_train, group):
         if epoch % 1 == 0:
              
             model.eval() # evaluate model
-            result, rmse = testing_func(num_test , group_test)  #run test through model
+            result, rmse = testing_func(num_test, group_test)  #run test through model
 
             if rmse_temp < rmse and rmse_temp <5:
                 result, rmse = result_temp, rmse_temp
@@ -84,13 +94,14 @@ def train(model, num_train, group):
 	
 if __name__ == '__main__': 
 	# import data
-    
-    input_size = 8 # shouldn't be hard coded
+    battery = "B0005"
+    input_size = 7 # shouldn't be hard coded
     n_hidden = input_size
     n_layer = 2
     n_epoch = 150
     lr = 0.01
-
+    test_size = 0.2
+    cv_size = 0.2
     # gpu?
 
     # LSTM Model initialization
@@ -99,5 +110,5 @@ if __name__ == '__main__':
     optimizer = torch.optim.Adam(model.parameters(), lr = lr)
 
     # training and evaltuation
-    result, rmse = train(model, num_train, group )
+    result, rmse = train(model, battery )
 
