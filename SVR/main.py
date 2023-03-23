2f1c47ae0c1f5114eb5372201288d9b85f675f4c
Greta <g.a.skorupska-ruiz@student.tudelft.nl>
2023-03-23 12:14:28 +0100

    model

--- SVR/main.py
+++ SVR/main.py
@@ -6,14 +6,6 @@ from pandas import *
 
 X_train, X_test, y_train, y_test, X, y = split_data(load_data("B0005"), 0.2)
 
-# X_train = X_train.to_numpy()
-# y_train = y_train.to_numpy()
-# X_test = X_test.to_numpy()
-# y_test = y_test.to_numpy()
-
-# X_train = X_train.reshape(-1, 1)
-# y_train = y_train.reshape(-1, 1)
-
 best_svr = SVR(C=20, epsilon=0.0001, gamma=0.00001, cache_size=200,
     kernel='rbf', max_iter=-1, shrinking=True, tol=0.001, verbose=False)
 
