c4f3569bcf676dbd479b7a2bc585b746559ae869
Greta <g.a.skorupska-ruiz@student.tudelft.nl>
2023-05-04 11:31:15 +0200

    pso stuff

---
+++ PytorchLSTM/import pyswarms.py
@@ -0,0 +1,112 @@
+import pyswarms
+import numpy
+from sketchymain import run_model
+
+import config
+import matplotlib.pyplot as plt
+
+#I USE PSO IN ORDER TO OPTIMIZE TWO PARAMETERS:
+    #NUMBER OF FILTERS USED ON CONVOLUTION LAYERS
+    #NUMBER OF EPOCHS USED IN TRAINING PROCESS
+
+def optimizeCNNLSTM(particleDimensions):
+
+    '''
+    This is loss function applied by all particles in iterations
+    :param x_train: samples used in train
+    :param x_test: samples used in test
+    :param y_train: targets used in train
+    :param y_test:  targets used in test
+    :param batch_size: integer that represents batch size
+    :param kernel_size: integer of tuple with only one integer (integer, ) --> length of convolution window
+    :param particleDimensions: numpy array with dimensions of a n particle --> 2 dimensions (filters, epochs)
+    :param stride: by default=1, integer represents stride length of convolution
+    :return: loss --> result of application of loss equation
+    '''
+
+    try:
+
+        #RETRIEVE DIMENSIONS OF PARTICLE
+    
+
+        #CALL CNN FUNCTION cnn --> RETURN accuracy
+        
+        #APPLY LOST FUNCTION --> THE MAIN OBJECTIVE IS TO MINIMIZE LOSS --> MAXIMIZE ACCURACY AND AT SAME TIME MINIMIZE THE NUMBER OF EPOCHS
+                                #AND FILTERS, TO REDUCE TIME AND COMPUTACIONAL POWER
+        loss = run_model(n_hidden=particleDimensions[0], n_layer=particleDimensions[1], n_epoch=particleDimensions[2], lr=particleDimensions[3], test_size=particleDimensions[4], cv_size=particleDimensions[5], seq=particleDimensions[6])
+       
+        return loss
+
+    except:
+        raise
+
+def particleIteration(particles):
+
+    '''
+    This is function that calls loss function, and returns all losses return by all particles on one iteration
+    :param x_train: samples used in train
+    :param x_test: samples used in test
+    :param y_train: targets used in train
+    :param y_test:  targets used in test
+    :param batch_size: integer that represents batch size
+    :param kernel_size: integer of tuple with only one integer (integer, ) --> length of convolution window
+    :param stride: by default=1, integer represents stride length of convolution
+    :param particles: numpy array --> (particles, dimensions)
+    :return: lossArray --> all losses returned by all particles
+    '''
+
+    try:
+
+        numberParticles = particles.shape[0]
+        allLosses = [optimizeCNNLSTM(particleDimensions=particles[i])for i in range(numberParticles)]
+
+        return allLosses#NEED TO RETURN THIS PYSWARMS NEED THIS
+
+    except:
+        raise
+
+def callCNNOptimization(numberParticles,bounds, **kwargs):
+
+    '''
+    This is the function that defines all PSO context and calls loss function for every particles (fill all iterations)
+    :param x_train: samples used in train
+    :param x_test: samples used in test
+    :param y_train: targets used in train
+    :param y_test:  targets used in test
+    :param batch_size: integer that represents batch size
+    :param kernel_size: integer of tuple with only one integer (integer, ) --> length of convolution window
+    :param stride: by default=1, integer represents stride length of convolution
+    :param numberParticles: integer --> number of particles of swarm
+    :param iterations: integer --> number of iterations
+    :param bounds: numpy array (minBound, maxBound) --> minBound: numpyArray - shape(dimensions), maxBound: numpyArray - shape(dimensions)
+    :return cost: integer --> minimum loss
+    :return pos: numpy array with n dimensions --> [filterValue, epochValue], with best cost (minimum cost)
+    :return optimizer: SWARM Optimization Optimizer USED IN DEFINITION AND OPTIMIZATION OF PSO
+    '''
+
+    try:
+
+        #GET PSO PARAMETERS
+        psoType = kwargs.get(config.TYPE)
+        options = kwargs.get(config.OPTIONS)
+
+        #DIMENSIONS OF PROBLEM
+        dimensions = 7
+
+        #OPTIMIZER FUNCTION
+        if psoType == config.GLOBAL_BEST:
+            optimizer = pyswarms.single.GlobalBestPSO(n_particles=numberParticles, dimensions=dimensions,
+                                                     options=options, bounds=bounds)
+        elif psoType == config.LOCAL_BEST:
+            optimizer = pyswarms.single.LocalBestPSO(n_particles=numberParticles, dimensions=dimensions,
+                                                     options=options, bounds=bounds)
+        else:
+            raise AttributeError
+        #GET BEST COST AND PARTICLE POSITION
+        cost, pos = optimizer.optimize(objective_func=particleIteration(numberParticles ))
+
+        return cost, pos, optimizer
+
+    except:
+        raise
+print(callCNNOptimization(50, 3))
---
+++ PytorchLSTM/sketchymain.py
@@ -0,0 +1,217 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from model import *
+import numpy as np
+from data_processing import load_gpu_data
+from torch.utils.data import DataLoader, TensorDataset
+# import plot
+import matplotlib.pyplot as plt
+def load_data_normalise(battery):
+    data = []
+    # for all battery files combine them into one dataframe
+    for i in battery:
+        data.append(pd.read_csv("data/" + i + "_TTD.csv"))
+    data = pd.concat(data)
+    # print(data)
+    # normalize the data
+    normalized_data = (data-data.mean(axis=0))/data.std(axis=0)
+
+
+    # for i in battery:
+    #     data += pd.read_csv("data/" + i + "_TTD.csv").to_numpy().tolist()
+    # print(f"data size {len(data)}")
+    # data = pd.DataFrame(data)
+    # normalized_data = (data-data.mean(axis=0))/data.std(axis=0)
+    return normalized_data
+
+
+
+
+def testing_func(X_test, y_test):
+    rmse_test, result_test = 0, list()
+
+    # one interation 
+    test_predict = model.forward(X_test)
+
+    y_pred = model(X_test)
+    rmse_test = np.sqrt(criterion(y_pred, y_test).item())
+    # for ite in range(1, num + 1):
+    #     X_test = group_for_test.get_group(ite).iloc[:, 2:]
+    #     X_test_tensors = torch.Tensor(X_test.to_numpy())
+    #     X_test_tensors = torch.reshape(X_test_tensors, (X_test_tensors.shape[0], 1, X_test_tensors.shape[1]))
+
+    #     test_predict = model.forward(X_test_tensors)
+    #     data_predict = max(test_predict[-1].detach().numpy(), 0)
+    #     result_test.append(data_predict)
+    #     rmse_test = np.add(np.power((data_predict - y_test.to_numpy()[ite - 1]), 2), rmse_test)
+
+    # rmse_test = (np.sqrt(rmse_test / num)).item()
+    return result_test, rmse_test
+
+class EarlyStopper:
+    def __init__(self, patience=1, min_delta=0.):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.min_validation_loss = np.inf
+
+    def early_stop(self, validation_loss):
+        """Implement the early stopping criterion.
+        The function has to return 'True' if the current validation loss (in the arguments) has increased
+        with respect to the minimum value of more than 'min_delta' and for more than 'patience' steps.
+        Otherwise the function returns 'False'."""
+        
+        if validation_loss < self.min_validation_loss:
+            self.min_validation_loss = validation_loss
+            return False
+        else:
+            if validation_loss - self.min_validation_loss > self.min_delta: #add code here
+                self. counter += 1
+                if self.counter > self.patience:
+                    return True 
+                else:
+                    return False
+
+            else:
+                return False          
+
+def train(model, X_train, y_train, X_val, y_val, n_epoch, lf, optimizer, es_patience, es_delta, verbose = True):
+    epoch = []
+    model.to(device) # set model to GPU
+    #intiate early stopper
+    early_stopper = EarlyStopper(patience=es_patience, min_delta=es_delta)
+    # X_train = X_train.double()
+    # y_train = y_train.double()
+    # X_val = X_val.double()
+    # y_val = y_val.double()
+
+    with torch.no_grad():
+        train_loss_history = []
+        val_loss_history = []
+
+    for i in range(n_epoch):
+        target_train = model(X_train).unsqueeze(2) # i changed this 
+        #target_train.reshape(4000, 10, 1)
+        target_val = model(X_val).unsqueeze(2) # i changed this - added the unsqueeze thing 
+        # print(f'size of target_train {target_train.shape} and size of y_train {y_train.shape}')
+        # print(f"x_train {X_train.shape} and y_train {y_train.shape}")
+        loss_train = lf(target_train, y_train)
+        loss_val = lf(target_val, y_val)
+        train_loss_history.append(loss_train.item())
+        val_loss_history.append(loss_val.item())
+
+        epoch.append(i+1)
+        optimizer.zero_grad()
+        loss_train.backward()
+        optimizer.step()
+
+        # if verbose:
+        print(f"Epoch {i+1}: train loss = {loss_train.item():.4f}, val loss = {loss_val.item():.4f}")
+
+        # if early_stopper.early_stop(loss_val.item()):
+        #     #print(f"Early stopping at epoch {i+1}")
+        #     break
+    return train_loss_history, val_loss_history, epoch
+
+# def train(model, battery):
+#     model.to(device) # set model to GPU
+#      # number of samples
+#      # grouped data per sample
+
+#     norm_data = load_data_normalise(battery)
+    
+#     X_train, y_train, X_test, y_test, X_cv, y_cv = load_gpu_data(norm_data, test_size=test_size, cv_size=cv_size)
+#     train_dataset = TensorDataset(X_train, y_train)
+#     print(y_train.shape)
+#     train_loader = DataLoader(train_dataset, batch_size=10, shuffle=True)
+#     val_dataset = TensorDataset(X_cv, y_cv)
+#     val_loader = DataLoader(val_dataset, batch_size=10)
+#     test_dataset = TensorDataset(X_test, y_test)
+#     test_loader = DataLoader(test_dataset, batch_size=10)
+#     # X = norm_data.drop("TTD", axis = 1)
+#     # y = norm_data["TTD"]
+#     # X_train, y_train, X_test, y_test, X_cv, y_cv  = train_test_validation_split(X, y, test_size, cv_size)
+#     num_train = len(X_train)
+#     rmse_temp = 1000
+#     epoch_loss = 0
+#     for epoch in range(n_epoch):
+#         # print(X_train[0]) this highlighets the issue that the data doesn't change so need to fix data loaders
+#         model.train() # set model to training mode
+#         optimizer.zero_grad() # calc and set grad = 0
+#         outputs = model(X_train) # forward pass
+#         print(f"size of output {outputs.shape}, size of y_train {y_train.shape}")
+#         loss = criterion(outputs, y_train) # calc loss for current pass
+#         loss = torch.unsqueeze(loss, 0) # add dimension to loss
+#         epoch_loss += loss.item() # add loss to epoch loss
+#         loss.backward() # update model parameters
+#         optimizer.step() # update loss func   
+        
+#         model.eval() # evaluate mode model (ie no drop out)
+#         result, rmse = testing_func(X_test, y_test)  #run test through model
+
+#         if rmse_temp < rmse and rmse_temp <0.5:
+#             result, rmse = result_temp, rmse_temp
+#             print("Early stopping ")
+#             break
+        
+#         rmse_temp, result_temp = rmse, result #store lst rmse
+#         print("Epoch: %d, loss: %1.5f, rmse: %1.5f" % (epoch, loss , rmse))
+
+#     return rmse, result
+def plot_loss(train_loss, val_loss, epoch):
+    plt.plot(epoch, train_loss, label='train loss')
+    plt.plot(epoch, val_loss, label='val loss')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.legend()
+    plt.show()
+
+
+
+def run_model(n_hidden, n_layer, n_epoch, lr, test_size, cv_size, seq):
+	# import data
+    battery = ['B0005', 'B0006', 'B0007', 'B0018']
+    data = load_data_normalise(battery)
+    input_size = data.shape[1] -1 #len(data.columns) - 1
+    n_hidden = 20 #input_size
+    n_layer = 2
+    n_epoch = 200
+    lr = 0.001
+    test_size = 0.1
+    cv_size = 0.1
+    seq = 50
+    # gpu?
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    #data initialization
+    X_train, y_train, X_val, y_val, X_test, y_test = load_gpu_data(data, test_size=test_size, cv_size=cv_size, seq_length=seq)  
+    print(X_train.dtype)
+    #where is X_train
+    print(f"x_train is on {X_train.device}, y_train is on {y_train.device}")
+
+    # LsTM Model initialization
+    # modellstm= LSTM1(input_size, n_hidden, n_layer, seq).double() # ahh i changed the seq len thing too 
+    model = CNNLSTM(input_size, seq, n_hidden, n_layer).double() 
+    
+    criterion = torch.nn.MSELoss() 
+    optimizer = torch.optim.Adam(model.parameters(), lr = lr)
+
+    
+    # training and evaltuation
+    train_hist, val_hist, epoch = train(model, X_train, y_train, X_val, y_val, n_epoch, criterion, optimizer, es_patience = 1e-16, es_delta = 1e-12, verbose = True)
+    predictions = model(X_test).to('cpu').detach().numpy()
+    plt.plot(predictions)
+    plt.plot(y_test.squeeze(2).to('cpu').detach().numpy())
+    plt.show()
+
+    loss = ((predictions - y_test.squeeze(2).to('cpu').detach().numpy()) ** 2).mean()
+    print(loss)
+    
+    import matplotlib.pyplot as plt
+
+    plt.plot(epoch, train_hist)
+    plt.plot(epoch, val_hist)
+    plt.show()
+    
+    print(model)
+    return loss
