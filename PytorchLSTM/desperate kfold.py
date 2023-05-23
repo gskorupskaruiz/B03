3b9c4319ee545e4ff086db0fcca4dac10cd6815d
Greta <g.a.skorupska-ruiz@student.tudelft.nl>
2023-05-23 14:40:03 +0200

    fixed kfold????

--- PytorchLSTM/desperate kfold.py
+++ PytorchLSTM/desperate kfold.py
@@ -1,6 +1,8 @@
+
+import pandas as pd
 from model import *
 import numpy as np
-from data_processing import load_gpu_data, load_gpu_data_with_batches
+
 import torch
 from torch.utils.data import Dataset
 import math 
@@ -8,45 +10,76 @@ from main import trainbatch, SeqDataset
 import matplotlib.pyplot as plt
 
 
-def load_data_normalise(battery):
+def load_data_normalise_cv(battery, which_model):
     data = []
     # for all battery files combine them into one dataframe
-    for i in battery:
-        data.append(pd.read_csv("data/" + i + "_TTD.csv"))
-    data = pd.concat(data)
+    
+    if which_model == "LSTM-CNN":
+        for i in battery:
+            data.append(pd.read_csv("data/" + i + "_TTD.csv"))
+        
+    elif which_model == "hybrid":
+        for i in battery:
+            data.append(pd.read_csv("data/" + i + "_TTD - with SOC.csv"))
+            
+    data = pd.concat(data)   
     # normalize the data
     normalized_data = (data-data.mean(axis=0))/data.std(axis=0)
     return normalized_data
 
+def load_gpu_data_with_batches_cv(data, seq_length, which_model):
+    
+    y = data["TTD"]
+    if which_model == "LSTM-CNN":
+        X = data.drop(["TTD"], axis=1)
+        input_lstm = 7
+    elif which_model == "hybrid":
+        X = data.drop(["TTD"], axis=1).drop(["Voltage_measured"], axis=1)
+        input_lstm = 8
+    x_tr = []
+    y_tr = []
+    for i in range(seq_length, len(X)):
+        x_tr.append(X.values[i-seq_length:i])
+        y_tr.append(y.values[i])
+		
+    x_tr = torch.tensor(np.array(x_tr))
+    y_tr = torch.tensor(y_tr).unsqueeze(1).unsqueeze(2)
+
+    if torch.cuda.is_available() == True:
+        print('Running on GPU')
+        X = x_tr.to('cuda').double()
+        y = y_tr.to('cuda').double()
+
+    else:
+        X = x_tr.clone().detach().double()
+        y = y_tr.clone().detach().double()
+
+    return X, y, input_lstm
 
 
-def run_model_cv(hyperparams):
+
+def run_model_cv(hyperparams, which_model, k_fold):
     
     all_losses = []
     
-    # SPECIFY K
-    k_fold = 4
+    all_batteries = ['B0005', 'B0006', 'B0007', 'B0018', 'B0029', 'B0031', 'B0032']
     
-    #IMPORTANT - IF YOU'RE RUNNING THIS FOR LSTM ONLY CHANGE THE Number OF LSTM INPUTS IN THE  model.py (LINE 243 AFAIK)
+    k_fold_batteries = all_batteries[:k_fold]
+   
+    kfold_test = []
+    kfold_train = []
     
-    if k_fold == 7:
-    # for 7-fold cross validation
-        kfold_test = [['B0032'],['B0031'], ['B0029'], ['B0018'], ['B0007'],['B0006'],['B0005']]
-        kfold_train = [['B0005', 'B0006', 'B0007', 'B0018', 'B0029', 'B0031'], ['B0005', 'B0006', 'B0007', 'B0018','B0029', 'B0032'], ['B0005', 'B0006', 'B0007', 'B0018', 'B0031', 'B0032'],['B0005', 'B0006', 'B0007', 'B0029', 'B0031', 'B0032'], ['B0005', 'B0006', 'B0018', 'B0029', 'B0031', 'B0032'], ['B0005', 'B0007', 'B0018', 'B0029', 'B0031', 'B0032'], ['B0006', 'B0007', 'B0018', 'B0029', 'B0031', 'B0032']]
-    elif k_fold == 4:
-    # for 4-fold cross validation
-        kfold_test = [['B0005'],['B0006'], ['B0007'], ['B0018']]
-        kfold_train = [['B0006', 'B0007', 'B0018'], ['B0005', 'B0007', 'B0018'], ['B0005', 'B0006', 'B0018'], ['B0005', 'B0006', 'B0007']]
-    
-    
-    battery = ['B0005', 'B0006', 'B0007', 'B0018', 'B0029', 'B0031', 'B0032']
-
     for i in range(k_fold):
+        kfold_test.append([k_fold_batteries[i]])
+        other_batteries = k_fold_batteries.copy()
+        other_batteries.remove(k_fold_batteries[i])
+        kfold_train.append(other_batteries)
+                
         battery = kfold_train[i]
-        test_battery = load_data_normalise(kfold_test[i])
-        
+        test_battery = load_data_normalise_cv(kfold_test[i], which_model)
+        data = load_data_normalise_cv(battery, which_model)
         print('Test battery:', kfold_test[i])
-        data = load_data_normalise(battery)
+        
 
         print(hyperparams)
         n_hidden, n_layer, lr, seq, batch_size, num_layers_conv, output_channels_val, kernel_sizes_val, stride_sizes_val, padding_sizes_val, hidden_size_lstm, num_layers_lstm, hidden_neurons_dense_val = hyperparams
@@ -60,15 +93,16 @@ def run_model_cv(hyperparams):
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
         #data initialization
-        X_train, y_train, X_test, y_test, X_val, y_val = load_gpu_data_with_batches(data, test_size=test_size, cv_size=cv_size, seq_length=seq)          
+        X_train, y_train, input_lstm = load_gpu_data_with_batches_cv(data, seq_length=seq, which_model=which_model)          
         
-        X_train, y_train, X_test, y_test, X_val, y_val = X_train.to(device), y_train.to(device), X_test.to(device), y_test.to(device), X_val.to(device), y_val.to(device)
+        X_train, y_train = X_train.to(device), y_train.to(device)
         
-        X_kfold, y_kfold, X_testkf, y_testkf, X_valkf, y_valkf = load_gpu_data_with_batches(test_battery, test_size=0.1, cv_size=0.1, seq_length=seq)
-        
+        X_test, y_test, _ = load_gpu_data_with_batches_cv(test_battery, seq_length=seq, which_model=which_model)
+
+        X_test, y_test = X_test.to(device), y_test.to(device)
         
         dataset = SeqDataset(x_data=X_train, y_data=y_train, seq_len=seq, batch=batch_size)
-        datasetv = SeqDataset(x_data=X_valkf, y_data=y_valkf, seq_len=seq, batch=batch_size)
+        datasetv = SeqDataset(x_data=X_test, y_data=y_test, seq_len=seq, batch=batch_size)
         # LsTM Model initialization
         
         output_channels = [output_channels_val] * num_layers_conv
@@ -77,19 +111,16 @@ def run_model_cv(hyperparams):
         padding_sizes = [padding_sizes_val] * num_layers_conv
         hidden_neurons_dense = [seq, hidden_neurons_dense_val, 1]
         
-        model = ParametricCNNLSTM(num_layers_conv, output_channels, kernel_sizes, stride_sizes, padding_sizes, hidden_size_lstm, num_layers_lstm, hidden_neurons_dense, seq).double()
+        model = ParametricCNNLSTM(num_layers_conv, output_channels, kernel_sizes, stride_sizes, padding_sizes, hidden_size_lstm, num_layers_lstm, hidden_neurons_dense, seq, input_lstm).double()
         model.train()
         model.to(device)
-        criterion = torch.nn.MSELoss() 
-        optimizer = torch.optim.Adam(model.parameters(), lr = lr)
 
         # training and evaltuation
-        train_hist, val_hist = trainbatch(model, dataset, datasetv, n_epoch, criterion, optimizer, verbose = True)
         model.eval()
         
-        predictions = model(X_kfold).to('cpu').detach().numpy()
+        predictions = model(X_test).to('cpu').detach().numpy()
     
-        loss = ((predictions.squeeze(2) - y_kfold.squeeze(2).to('cpu').detach().numpy()) ** 2).mean()
+        loss = ((predictions.squeeze(2) - y_test.squeeze(2).to('cpu').detach().numpy()) ** 2).mean()
 
         if loss > 0.5:
             print(f'Loss is greater than 0.5 at {i+1}th cross validation, stopping iteration')
@@ -131,7 +162,14 @@ def run_model_cv(hyperparams):
 
     return loss
 
+"""
+Define the hyperparameters to be tested
+
+(comment this out if you're optimising the hyperparameters)
+
+"""
 
 
 testing_hyperparameters = [120, 2, 30, 8, 800, 1, 7, 1, 2, 1, 50, 7, 1]
-print(run_model_cv(testing_hyperparameters))
+
+print(run_model_cv(testing_hyperparameters, 'LSTM-CNN', 4))
