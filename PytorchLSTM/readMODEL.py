23b9062c0c1ed56247c70b16257f450a68dbaaee
AlexisVandenHeede <105492216+AlexisVandenHeede@users.noreply.github.com>
2023-06-05 16:16:44 +0200

    saved models, started working on generating plots, fixed early stopper

--- PytorchLSTM/readMODEL.py
+++ PytorchLSTM/readMODEL.py
@@ -1,18 +1,64 @@
 import matplotlib.pyplot as plt
-from model import ParametricCNNLSTM
+from model import ParametricLSTMCNN
 import torch
-from bitstring import BitArray
+from desperate_kfold import load_data_normalise_cv
+from sklearn.model_selection import train_test_split
 
-def read_model(model_name):
-    model = torch.load(f'PytorchLSTM\{model_name}')
+def read_model(model_name, hyperparameters):
+    lr, seq, batch_size, num_layers_conv, output_channels, kernel_sizes, stride_sizes, padding_sizes, hidden_size_lstm, num_layers_lstm, hidden_neurons_dense = hyperparameters
+    if model_name == 'hybrid':
+        input_lstm = 8
+    elif model_name == 'LSTN-CNN':
+        input_lstm = 7
+    else:
+        print('Invalid model name')
+        print(f'model name: {model_name}')
+        return
+    model = ParametricLSTMCNN(num_layers_conv, output_channels, kernel_sizes, stride_sizes, padding_sizes, hidden_size_lstm, num_layers_lstm, hidden_neurons_dense, seq, input_lstm)
+    model.load_state_dict(torch.load(f'C:/Users/alexi/OneDrive - Delft University of Technology/BSc2 AE/Q3/Project/B03/PytorchLSTM/'+model_name+'model.pt'))
+    # PytorchLSTM\LSTM-CNNmodel.pt
     model.eval()
+    print(f'Loaded model: {model_name}')
     return model
 
-def generate_prediction(model, X_test):
-    prediction = model(X_test)
-    return prediction
+def load_data_kfold(k, battery, which_model):
+    all_batteries = ['B0005', 'B0006', 'B0007', 'B0018', 'B0029', 'B0031', 'B0032']
+    K_fold_batteries = all_batteries[:k]
+    normalized_test_data = load_data_normalise_cv(K_fold_batteries[battery], which_model)
+    rest_of_batteries = K_fold_batteries.drop(K_fold_batteries[battery])
+    normalized_train_data = load_data_normalise_cv(rest_of_batteries, which_model)
+    y = normalized_train_data["TTD"]
+    y_test = normalized_test_data["TTD"]
+    X = normalized_test_data.drop(columns=["TTD"])
+    X_test = normalized_test_data.drop(columns=["TTD"])
+    if which_model == 'hybrid':
+        X = X.drop(columns=["Voltage_measured"])
+        X_test = X_test.drop(columns=["Voltage_measured"])
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.1, shuffle=False)
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
+def k_fold_plots(k_fold, model, which_model):
+    all_batteries = ['B0005', 'B0006', 'B0007', 'B0018', 'B0029', 'B0031', 'B0032']
+    k_fold_batteries = all_batteries[:k_fold]
+    predictions_test = []
+    
+    for battery in k_fold_batteries:
+        X_train, y_train, X_val, y_val, X_test, y_test = load_data_kfold(k_fold, battery, which_model)
+        predictions_test.append(model(X_test))
+    
 
 
+def scatter_plot(x, y, x_label, y_label):
+    plt.scatter(x, y)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.show()
+    return x, y, x_label, y_label
+
+def save_plot(plot_func, name):
+    x, y, x_label, y_label = plot_func
+    plt.savefig(f'{name}.png')
+    
 
 def plot_loss(epoch, train_loss, val_loss):
     plt.plot(epoch, train_loss, label='Training loss')
@@ -32,61 +78,7 @@ def plot_prediction(prediction, y_actual):
 
 # read_model('hyrbidmodel')
 
+testing_hyperparameters = [0.00284, 7, 681, 4, [1, 3, 5, 8], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], 20, 2, [1, 2, 4, 6, 1]] # best data driven
+testing_hyperparameters =  [0.00167, 8, 2000, 5, [1, 9, 18, 27, 36], [1, 5, 2.0, 7.0, 9.0], [1, 1, 1, 1, 1], [1, 1, 2, 3, 4], 14, 3, [24, 18, 12, 6, 1]] # best hybrid
 
-ga_individual_solution = [0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0]
-
-gene_length = 3
-    # decode GA solution to get hyperparamteres
-lstm_layers_bit = BitArray(ga_individual_solution[0:gene_length]) # don't understand the bitarray stuff yet or the length given per hyperparameter
-lstm_neurons_bit = BitArray(ga_individual_solution[gene_length:2*gene_length])
-lstm_sequential_length_bit = BitArray(ga_individual_solution[2*gene_length:3*gene_length])
-learning_rate_bit = BitArray(ga_individual_solution[3*gene_length:4*gene_length])
-cnn_layers_bit = BitArray(ga_individual_solution[4*gene_length:5*gene_length])
-cnn_kernel_size_bit = BitArray(ga_individual_solution[5*gene_length:6*gene_length])
-cnn_stride_bit = BitArray(ga_individual_solution[6*gene_length:7*gene_length])
-cnn_padding_bit = BitArray(ga_individual_solution[7*gene_length:8*gene_length])
-cnn_output_size_bit = BitArray(ga_individual_solution[8*gene_length:9*gene_length])
-hidden_neurons_dense_bit = BitArray(ga_individual_solution[9*gene_length:10*gene_length])
-batch_size_bit = BitArray(ga_individual_solution[10*gene_length:11*gene_length])
-
-lstm_layers = lstm_layers_bit.uint
-lstm_sequential_length = lstm_sequential_length_bit.uint
-lstm_neurons = lstm_neurons_bit.uint
-learning_rate = learning_rate_bit.uint
-cnn_layers = cnn_layers_bit.uint
-cnn_kernel_size = cnn_kernel_size_bit.uint
-cnn_stride = cnn_stride_bit.uint
-cnn_padding = cnn_padding_bit.uint
-cnn_output_size = cnn_output_size_bit.uint
-hidden_neurons_dense = hidden_neurons_dense_bit.uint
-
-batch_size = batch_size_bit.uint
-
-# resize hyperparameters
-lstm_layers += 1
-lstm_sequential_length += 1
-lstm_neurons += 1
-learning_rate += 1
-cnn_layers += 1
-cnn_kernel_size += 1
-cnn_stride += 1
-cnn_padding += 1
-cnn_output_size += 1
-hidden_neurons_dense += 1
-batch_size += 1
-learning_rate = learning_rate/100
-batch_size = batch_size * 100
-lstm_neurons *= 10 
-
-
-print(f"lstm Layers =  {lstm_layers}")
-print(f"lstm Sequential Length =  {lstm_sequential_length}")
-print(f"lstm Neurons =  {lstm_neurons}")
-print(f"learning rate =  {learning_rate}")
-print(f"cnn layers =  {cnn_layers}")
-print(f"cnn kernel size =  {cnn_kernel_size}")
-print(f"cnn stride =  {cnn_stride}")
-print(f"cnn padding =  {cnn_padding}")
-print(f"cnn neurons =  {cnn_output_size}")
-print(f"hidden neurons =  {hidden_neurons_dense}")
-print(f"batch size =  {batch_size}")
+model = read_model('hybrid', testing_hyperparameters)
